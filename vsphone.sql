a6d97161ec1177b6ef0907bfbd788b476c8a93c6
qingqingcaoyuanlin <1451368913@qq.com>
2016-10-23 15:36:20 +0800

    修改数据库sql文件

--- vsphone.sql
+++ vsphone.sql
@@ -1,15 +1,31 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : vsphone
+Source Server Version : 60011
+Source Host           : localhost:3306
+Source Database       : vsphone
+
+Target Server Type    : MYSQL
+Target Server Version : 60011
+File Encoding         : 65001
+
+Date: 2016-10-22 12:42:27
+*/
+
 SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 -- Table structure for `device`
 -- ----------------------------
 DROP TABLE IF EXISTS `device`;
 CREATE TABLE `device` (
-  `DeviceNum` char(20) DEFAULT NULL,
-  `DeviceType` char(20) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ProjectCode` char(20) NOT NULL DEFAULT '',
   `Header` char(60) DEFAULT NULL,
-  `ProjectCode` char(20) DEFAULT NULL,
-  PRIMARY KEY  (`ProjectCode`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `DeviceType` char(20) DEFAULT NULL,
+  `DeviceNum` char(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of device
@@ -20,16 +36,16 @@ CREATE TABLE `device` (
 -- ----------------------------
 DROP TABLE IF EXISTS `record`;
 CREATE TABLE `record` (
-  `ProjectCode` char(20) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ProjectCode` char(20) DEFAULT '',
   `Header` char(60) DEFAULT NULL,
   `DeviceType` char(20) DEFAULT NULL,
   `DeviceNum` char(20) DEFAULT NULL,
   `Time` datetime DEFAULT NULL,
   `SetPeriod` int(1) DEFAULT NULL,
   `CallPeriod` int(1) DEFAULT NULL,
-  `Succeed` bit(1) DEFAULT NULL,  
-  `deviceaa` int(1) DEFAULT NULL,
-  PRIMARY KEY  (`ProjectCode`)
+  `Succeed` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
