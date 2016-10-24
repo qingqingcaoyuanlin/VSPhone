ef48b2698573ca45cb8ceec4d3c5d01c70036d77
qingqingcaoyuanlin <1451368913@qq.com>
2016-10-25 00:14:18 +0800

    设备数据库修改，未更新到自动呼叫的列表

--- VSPhone/DeviceRecord.cs
+++ VSPhone/DeviceRecord.cs
@@ -17,47 +17,91 @@ namespace VSPhone
             InitializeComponent();
             InitSetting();
         }
-        private DataTable dt = new DataTable();
-        private MySqlDataAdapter adapter;
-        private MySqlCommand cmd = new MySqlCommand();
-        private MySqlConnection connect = new MySqlConnection();
-        private String strCmd;
+        private DataTable dataTable = new DataTable();
+//        private MySqlDataAdapter adapter;
+//        private MySqlCommand cmd = new MySqlCommand();
+//        private MySqlConnection connect = new MySqlConnection();
+//        private String strCmd;
         private void InitSetting()
         {
             //dt = DataBase.queryTable(DataBase.DeviceTable);
-            connect = DataBase.GetMySqlConnection();
-            strCmd = "select * from " + DataBase.DeviceTable;
-            adapter = new MySqlDataAdapter(strCmd, connect);
-            adapter.Fill(dt);
-            dataGridView1.DataSource = dt;
+            MySqlConnection connect = DataBase.GetMySqlConnection();
+            string strCmd = "select * from " + DataBase.DeviceTable;
+            MySqlDataAdapter adapter = new MySqlDataAdapter(strCmd, connect);
+            adapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
             
         }
 
         private void button1_Click(object sender, EventArgs e)  //增加
         {
+            /*
+            string cmd = "";
+            adapter.InsertCommand = DataBase.GetMySqlCommand(cmd, connect);
+            //adapter.InsertCommand = new MySqlCommand();
+            adapter.InsertCommand.Parameters.Add();
+             */
+            /*
+            string[] str = new string[dataGridView1.Rows.Count];
+            int index = 0;
+            
+            foreach(DataGridViewRow row in dataGridView1.Rows)
+            {
+                if(true)
+                {
+                    continue;
+                }
+                else
+                {
+                    str[index] = "insert into " + DataBase.DeviceTable + " values(null,'" + row.Cells[1].Value + "," + row.Cells[2].Value + "," + row.Cells[3].Value + "," + row.Cells[4].Value + "')";
+                    index++;
 
-        }
+                }
+                
+
+
+            }
+             */ 
+       }
 
         private void button2_Click(object sender, EventArgs e)  //删除
         {
+            MySqlConnection connect = DataBase.GetMySqlConnection();
+            string strCmd = "select * from " + DataBase.DeviceTable;
+            MySqlDataAdapter adapter = new MySqlDataAdapter(strCmd, connect);
+
             int row = dataGridView1.CurrentRow.Index;
-            DataRow dr = dt.Rows[row];
+            DataRow dr = dataTable.Rows[row];
             dr.Delete();
             MySqlCommandBuilder mscb = new MySqlCommandBuilder(adapter);
-            adapter.Update(dt);
+            adapter.Update(dataTable);
 
         }
 
-        private void button3_Click(object sender, EventArgs e)  //刷新
+        private void button3_Click(object sender, EventArgs e)  //修改/或者添加
         {
+            MySqlConnection connect = DataBase.GetMySqlConnection();
+            string strCmd = "select * from " + DataBase.DeviceTable;
+            MySqlDataAdapter adapter = new MySqlDataAdapter(strCmd, connect);
+
             MySqlCommandBuilder mscb = new MySqlCommandBuilder(adapter);
-            adapter.Update(dt);
+            adapter.Update(dataTable);
         }
 
         private void button4_Click(object sender, EventArgs e)  //清空
         {
-
+            string cmd = "truncate table "+ DataBase.DeviceTable;
+            if(DataBase.ExcuteCMD(cmd))
+            {
+                MessageBox.Show("清空成功");
+                dataTable.Clear();
+            }
+            else
+            {
+                MessageBox.Show("清空失败");
+            }
         }
     }
 }
